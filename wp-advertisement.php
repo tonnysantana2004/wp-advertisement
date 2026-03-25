9027f5bd660f5ae342bc55e729d0ca16ec77b757
Tonny Santana <tonnysantanasantos@gmail.com>
2026-03-25 05:24:00 -0300

    first commit

---
+++ wp-advertisement.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Advertisement
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-advertisement
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Advertisement
+ */
+
+// Your code starts here.
